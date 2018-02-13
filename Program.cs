9e28a4c9ff98cb33931dbe114e05bf20a9cbc2d4
JaminROCK <KingJamin@hotmail.com>
2018-02-13 14:30:44 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//^--- Visual Studio Imports---•
 
 //•---- Import SqlClient    ---v
 using System.Data.SqlClient;
